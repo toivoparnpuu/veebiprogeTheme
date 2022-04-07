edeb8e4d01b70bb6c9484d4aeca0af1f72af2c4f
Toivo Pärnpuu <toivo.parnpuu@tlu.ee>
2022-04-07 22:37:25 +0300

    section to div

--- index.php
+++ index.php
@@ -4,11 +4,11 @@ if ( have_posts() ) {
 while ( have_posts() ) {
 the_post(); ?>
 <h2><?php the_title(); ?></h2>
-<section class="sisu">
+<div class="subpagecontainter">
 
 <?php the_excerpt();?>
 <a href="<?php the_permalink(); ?>"> Loe rohkem </a>
-</section>
+</div>
 <?php
     }
   }
