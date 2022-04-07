5cfb4db12a1ccbcc3b956ab645d295212e2ecf67
Toivo Pärnpuu <toivo.parnpuu@tlu.ee>
2022-04-07 22:35:29 +0300

    sisu klassi lisamine

--- index.php
+++ index.php
@@ -4,8 +4,11 @@ if ( have_posts() ) {
 while ( have_posts() ) {
 the_post(); ?>
 <h2><?php the_title(); ?></h2>
+<section class="sisu">
+
 <?php the_excerpt();?>
 <a href="<?php the_permalink(); ?>"> Loe rohkem </a>
+</section>
 <?php
     }
   }
