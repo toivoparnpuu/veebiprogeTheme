bda579b275526ba5e45057c16963560b9874b7b2
Toivo Pärnpuu <toivo.parnpuu@tlu.ee>
2022-04-29 16:19:29 +0300

    header php refactoring
    https://codex.wordpress.org/Theme_Development search Document Head (header.php)

--- header.php
+++ header.php
@@ -1,8 +1,16 @@
-<head>
-<?php wp_head(); ?>
-</head>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <title><?php wp_title(); ?></title>
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+        <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
+        <?php wp_head(); ?>
+    </head>
+
 <header>
     <h1><?php echo get_bloginfo('name');?> <h1>
     <?php wp_nav_menu(array('theme_location' => 'links-menu', 'menu_class' => 'myNavList'));?>
     <?php wp_nav_menu(array('theme_location' => 'extra-menu', 'menu_class' => 'myNavListCss'));?>
-</header>
+
