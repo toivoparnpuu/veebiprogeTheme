e8f7948f3cc2237e653076ce87cf05ddd28622ee
Toivo Pärnpuu <toivo.parnpuu@tlu.ee>
2022-04-29 16:02:46 +0300

    head ploki nihutamine

--- header.php
+++ header.php
@@ -1,10 +1,9 @@
 <head>
 <?php wp_head(); ?>
 </head>
-
-</html>
 <header>
     <h1><?php echo get_bloginfo('name');?> <h1>
     <?php wp_nav_menu(array('theme_location' => 'links-menu', 'menu_class' => 'myNavList'));?>
     <?php wp_nav_menu(array('theme_location' => 'extra-menu', 'menu_class' => 'myNavListCss'));?>
 </header>
+</html>
