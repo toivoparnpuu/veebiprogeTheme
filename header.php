4d2f849f0268c90d4ab8555fb77ef44e79e3db02
Toivo Pärnpuu <toivo.parnpuu@tlu.ee>
2022-04-30 10:12:06 +0300

    <title> tag fix
    proovisin siin soovitatud lahendust https://stackoverflow.com/questions/9055009/wordpress-wp-title-blank-on-index-page

--- header.php
+++ header.php
@@ -2,7 +2,8 @@
 <html <?php language_attributes(); ?>>
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>" />
-        <title><?php wp_title(''); ?></title>
+        <title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
+        
         <link rel="profile" href="http://gmpg.org/xfn/11" />
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
         <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
