9b07444986eb63c26961b908ec9ee33273cfb8e9
Toivo Pärnpuu <toivo.parnpuu@tlu.ee>
2022-05-14 10:26:20 +0300

    rearange div tags

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
 <?php
 get_header();
 if ( have_posts() ) {
+?>
+<div class="subpagecontainter">
+<?php
 while ( have_posts() ) {
-  the_post(); ?>
+  the_post(); ?> 
+  
   <h2><?php the_title(); ?></h2>
-  <div class="subpagecontainter">
+  
     <div>
     <?php the_excerpt();?>
     <a href="<?php the_permalink(); ?>"> Loe rohkem </a>
     </div>
-  </div>
+  
   <?php
     }
+  ?>
+  </div>
+  
+  <?php
   }
   ?>
 <?php
