b72006f48e825048649c44a704744b3b30fdbb06
Toivo Pärnpuu <toivo.parnpuu@tlu.ee>
2022-05-14 10:38:59 +0300

    formating

--- single.php
+++ single.php
@@ -1,13 +1,22 @@
 <?php 
-get_header();
-the_title();
-<div class="subpagecontainter">
-    the_content();
-</div>
+    get_header();
+    the_title();
 ?>
+
 <div class="subpagecontainter">
-<div>Kõrgus: <?php echo(get_post_meta(get_the_ID(), $key = 'height', $single = true)); ?> </div>
+
+<?php
+    the_content();
+?>
 </div>
+<div class="subpagecontainter">
+<div>Kõrgus: 
+    <?php 
+    echo(get_post_meta(get_the_ID(), $key = 'height', $single = true)); 
+    ?> 
+</div>
+</div>
+
 <?php
 get_footer();
 ?>
