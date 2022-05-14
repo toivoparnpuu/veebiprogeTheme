a17ff1d1424fabeb196169e33d9fd1274577651f
Toivo Pärnpuu <toivo.parnpuu@tlu.ee>
2022-05-14 11:31:48 +0300

    header layout

--- header.php
+++ header.php
@@ -10,8 +10,8 @@
         <?php wp_head(); ?>
     </head>
 <body>
-<h1><?php echo get_bloginfo('name');?> </h1>
 <header>
     <?php wp_nav_menu(array('theme_location' => 'links-menu', 'menu_class' => 'myNavList'));?>
+    <h1><?php echo get_bloginfo('name');?> </h1>
     <?php wp_nav_menu(array('theme_location' => 'extra-menu', 'menu_class' => 'myNavListCss'));?>
 </header>
