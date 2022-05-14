9937ff943f1882adb5fad801dfebcf6816099b52
Toivo Pärnpuu <toivo.parnpuu@tlu.ee>
2022-05-14 10:44:35 +0300

    rearanged h2

--- index.php
+++ index.php
@@ -6,10 +6,8 @@ if ( have_posts() ) {
 <?php
 while ( have_posts() ) {
   the_post(); ?> 
-  
-  <h2><?php the_title(); ?></h2>
-  
     <div>
+    <h2><?php the_title(); ?></h2>
     <?php the_excerpt();?>
     <a href="<?php the_permalink(); ?>"> Loe rohkem </a>
     </div>
