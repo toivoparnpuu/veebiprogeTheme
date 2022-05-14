8a755dad8423602994381ed1173efc2b347cf6b2
Toivo Pärnpuu <toivo.parnpuu@tlu.ee>
2022-05-14 12:14:41 +0300

    custom fields

--- single.php
+++ single.php
@@ -15,6 +15,21 @@
     echo(get_post_meta(get_the_ID(), $key = 'height', $single = true)); 
     ?> 
 </div>
+<div>Asukoht: 
+    <?php 
+    echo(get_post_meta(get_the_ID(), $key = 'location', $single = true)); 
+    ?> 
+</div>
+<div>Kivi tüüp: 
+    <?php 
+    echo(get_post_meta(get_the_ID(), $key = 'rocktype', $single = true)); 
+    ?> 
+</div>
+<div>Radade arv: 
+    <?php 
+    echo(get_post_meta(get_the_ID(), $key = 'routes', $single = true)); 
+    ?> 
+</div>
 </div>
 
 <?php
